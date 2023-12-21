a4105491b392bad4054ad98a411d160dde99b077
yejin25 <yjin9187@naver.com>
2023-12-21 20:37:34 +0900

    add route_table

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source = "./modules/vpc"
-  vpc_name = "hairpin-vpc"
+  vpc_name = "hairpin-project-vpc"
   vpc_cidr = "10.0.0.0/24"
-  # nat_count = local.public_subnet_count
-  # public_subnet_id = module.subnet.public_subnet_ids
+  nat_count = 2
+  public_subnet_id = flatten(module.subnet[*].public_subnet_ids)
 }
 
 module "subnet" {
@@ -18,3 +18,31 @@ module "subnet" {
   subnet_az = data.aws_availability_zones.available.names[count.index]
 }
 
+module "public_route_tb" {
+  source = "./modules/routetb"
+  vpc_id = module.vpc.vpc_id
+  count = length(flatten(module.subnet[*].public_subnet_ids))
+  rt_name = flatten(module.subnet[*].public_subnet_names)[count.index]
+  subnet_id = flatten(module.subnet[*].public_subnet_ids)[count.index]
+
+  routings = {
+        igw = {
+            dst_cidr = "0.0.0.0/0",
+            dst_id = module.vpc.igw_id
+        }
+    }
+}
+
+module "private_route_tb" {
+  source = "./modules/routetb"
+  vpc_id = module.vpc.vpc_id
+  count = length(flatten(module.subnet[*].private_subnet_ids))
+  rt_name = flatten(module.subnet[*].private_subnet_names)[count.index]
+  subnet_id = flatten(module.subnet[*].private_subnet_ids)[count.index]
+  routings = {
+        nat = {
+            dst_cidr = "0.0.0.0/0",
+            dst_id = flatten(module.subnet[*].private_subnet_az)[count.index] == data.aws_availability_zones.available.names[0] ? module.vpc.nat_id[0] : module.vpc.nat_id[1]
+        }
+    }
+}
--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -5,3 +5,15 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private-subnet[*].id
 }
+
+output "public_subnet_names" {
+  value = aws_subnet.public-subnet[*].tags.Name
+}
+
+output "private_subnet_names" {
+  value = aws_subnet.private-subnet[*].tags.Name
+}
+
+output "private_subnet_az" {
+  value = aws_subnet.private-subnet[*].availability_zone
+}
