1e7a6e97c02555627312f81e259f339d94a1ec86
yejin25 <yjin9187@naver.com>
2023-12-27 13:51:21 +0900

    delete comment

--- main.tf
+++ main.tf
@@ -47,39 +47,39 @@ module "private_route_tb" {
   }
 }
 
-# module "eks" {
-#   source                         = "terraform-aws-modules/eks/aws"
-#   version                        = "~> 19.0"
-#   cluster_name                   = "hairpin-cluster"
-#   cluster_version                = "1.28"
-#   cluster_endpoint_public_access = true
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent = true
-#     }
-#   }
+module "eks" {
+  source                         = "terraform-aws-modules/eks/aws"
+  version                        = "~> 19.0"
+  cluster_name                   = "hairpin-cluster"
+  cluster_version                = "1.28"
+  cluster_endpoint_public_access = true
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
-#   vpc_id                   = module.vpc.vpc_id
-#   subnet_ids               = local.subnet_eks_nodegroup_ids
-#   control_plane_subnet_ids = local.subnet_eks_cluster_ids
-#   # EKS Managed Node Group(s)
-#   eks_managed_node_groups = {
-#     nodegroup = {
-#       min_size       = 2
-#       max_size       = 4
-#       desired_size   = 2
-#       instance_types = ["t3.medium"]
-#     }
-#   }
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = local.subnet_eks_nodegroup_ids
+  control_plane_subnet_ids = local.subnet_eks_cluster_ids
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    nodegroup = {
+      min_size       = 2
+      max_size       = 4
+      desired_size   = 2
+      instance_types = ["t3.medium"]
+    }
+  }
 
-#   //  remote_access = {
-#   //    ec2_ssh_key               = module.key_pair.key_pair_name
-#   //    source_security_group_ids = [aws_security_group.remote_access.id]
-#   //  }
-# }
+  //  remote_access = {
+  //    ec2_ssh_key               = module.key_pair.key_pair_name
+  //    source_security_group_ids = [aws_security_group.remote_access.id]
+  //  }
+}
